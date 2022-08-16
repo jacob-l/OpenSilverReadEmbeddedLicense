fefbcaac8dbf398d4a16d54d9d67b8053ead8e93
Iakov Lilo <jlilo@hwdtech.com>
2022-08-16 16:20:41 +0600

    Add project files.

---
+++ OpenSilverReadEmbeddedLicense/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverReadEmbeddedLicense
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+
+            var resourceName = "EmbeddedFile.txt";
+            var ea = System.Reflection.Assembly.GetEntryAssembly();
+            if (ea == null) return;
+
+            using (var stream = ea.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) return;
+
+                using (var sr = new StreamReader(stream))
+                {
+                    TextBlock1.Text = sr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
